a2a38c575a185b7c8abfb899e0a33b3ae0a1fc84
Архипенко Игорь <i.arkhipenko@domain.local>
2017-06-09 17:22:53 +0300

    Avoiding state mutations in reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'c92487ea1fcc254e3094a3344a26ec64';
-// http://samples.openweathermap.org/data/2.5/forecast?q=Moscow,ru&appid=c92487ea1fcc254e3094a3344a26ec64
+// http://api.openweathermap.org/data/2.5/forecast?q=Moscow,ru&appid=c92487ea1fcc254e3094a3344a26ec64
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
